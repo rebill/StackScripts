88b9f2f7056cc5d46ff36274f9a327fe5bc83349
weibiao.ruan <weibiao.ruan@renren-inc.com>
2013-09-18 15:13:24 +0800

    fix yum -y

--- centos/4-php-ext.sh
+++ centos/4-php-ext.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function installPhalcon() {
-    yum install php-devel
+    yum install -y php-devel
     git clone git://github.com/phalcon/cphalcon.git
     cd cphalcon/build
     sh ./install
