f5dc10397fbf31057710f53aa211aa7a5891b2dc
weibiao.ruan <weibiao.ruan@renren-inc.com>
2013-09-18 15:08:33 +0800

    add Phalcon ext

--- centos/4-php-ext.sh
+++ centos/4-php-ext.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-function 
+function installPhalcon() {
+    yum install php-devel
+    git clone git://github.com/phalcon/cphalcon.git
+    cd cphalcon/build
+    sh ./install
+}
+
+
+installPhalcon
